a3bcf3edf745f5169cbd35da25da58f19bcdc5fc
Theut94 <Mikkeltheut@gmail.com>
2022-11-21 11:11:30 +0100

    Created Core Entities

---
+++ Core/Entities/EventSlot.cs
@@ -0,0 +1,12 @@
+﻿namespace Core;
+
+public class EventSlot
+{
+    public int Id { get; set; }
+    public Event Event { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public List<SlotAnswer> SlotAnswers{ get; set; }
+    public Boolean Confirmed { get; set; } = false;
+
+}
---
+++ Core/Entities/SlotAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace Core;
+
+public class SlotAnswer
+{
+    public int Id { get; set; }
+    public EventSlot EventSlot { get; set; }
+    public string Answer { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
+
